f32aaf31e7ca3e8cb1bae87444f5aeedf47b1c60
Aran Pando <aran.pando@ymlgroup.com.au>
2019-06-24 16:23:31 +1000

    Added name attribute to MembershipType and updated MermbershipTypes table with the names of the types

--- VidlyRedo/Models/MembershipType.cs
+++ VidlyRedo/Models/MembershipType.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,9 @@ namespace VidlyRedo.Models
     public class MembershipType
     {
         public byte Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public String Name { get; set; }
         public short SignUpFee { get; set; }
         public byte DurationMonth { get; set; }
         public byte DiscountRate { get; set; }
